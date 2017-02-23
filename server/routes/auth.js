8c51891833d1dec282d6b8f99d3c0e835be86d66
Maris Usis <marisusisme@gmail.com>
2017-02-23 10:30:43 -0500

    fixed auth router file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function(passport) {
-	app.get('/', passport.authenticate('soundcloud-token'), function(req, res) {
+	router.get('/', passport.authenticate('soundcloud-token'), function(req, res) {
 		console.log(req);
 	});
 
