f6c2fc93faacc1c9b868f5b7d65afc56e34fce83
Maris Usis <marisusisme@gmail.com>
2017-03-24 15:03:00 -0400

    asdf

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,10 +5,10 @@ var path = require('path');
 
 console.log(__dirname);
 module.exports = function(passport) {
-
-	router.get('/js/*', function(req, res) {
-		res.sendFile(path.resolve(__dirname + '/../../dist/' + req.params[0]));
-	});
+	// 
+	// router.get('/js/*', function(req, res) {
+	// 	res.sendFile(path.resolve(__dirname + '/../../dist/' + req.params[0]));
+	// });
 
 	router.get('/*', function(req, res) {
 		res.sendFile(path.resolve(__dirname + '/../../client/' + req.path));
